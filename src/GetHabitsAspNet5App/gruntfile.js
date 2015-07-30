02f7a2eeac2449270da93e09e5c2ba0d44b9656b
Boyarincev Stas <boyarincev@gmail.com>
2015-07-30 21:23:52 +0300

    part 1 final

---
+++ src/GetHabitsAspNet5App/gruntfile.js
@@ -0,0 +1,28 @@
+﻿/// <binding ProjectOpened='default' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    //загружаем плагины Grunt
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //настраиваем плагины
+    grunt.initConfig({
+        uglify: {
+            scripts: {
+                files: { 'wwwroot/app.js': ['Scripts/app.js', 'Scripts/**/*.js'] }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['Scripts/**/*.js'],
+                tasks: ['uglify']
+            }
+        }
+    });
+
+    //настроим запуск задач
+    grunt.registerTask('default', ['uglify', 'watch']);
+};
