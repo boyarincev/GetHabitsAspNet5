631034c3a4dbb8425652cced46f406f7c1ef52f4
Boyarincev Stas <boyarincev@gmail.com>
2015-10-06 22:21:28 +0300

    rename Test Class for routing, delete api folder

--- GetHabitsAspNet5App.Tests/RoutingTests.cs
+++ GetHabitsAspNet5App.Tests/RoutingTests.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Builder;
 
 namespace GetHabitsAspNet5App.Tests
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Class1
+    public class RoutingTests
     {
 
         [Fact]
-        public void UniTest1()
+        public void AppRouteTest()
         {
-            Assert.True(true);
+            //var serviceCollection = new ServiceCollection();
+            //var applicationBuilder = new Appl
         }
     }
 }
