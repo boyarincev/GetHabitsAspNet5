bf807e78b872d4db27e6bde831153addb9ca190d
Boyarincev Stas <boyarincev@gmail.com>
2015-11-06 23:39:07 +0300

    added ApplicationHelper

---
+++ src/GetHabitsAspNet5App/Helpers/ApplicationHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetHabitsAspNet5App.Helpers
+{
+    public class ApplicationHelper
+    {
+        /// <summary>
+        /// Type of Claim contains UserId
+        /// </summary>
+        public string TypeClaimUserId = "UserId";
+
+        /// <summary>
+        /// Maximum count checkins, what can queried from HabitServise
+        /// </summary>
+        public int MaxCheckinCount = 30;
+    }
+}
